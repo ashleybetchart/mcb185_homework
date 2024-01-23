ec7b7d63c86cb2dc2de821ba60789a191d6f67af
ashleybetchart <asbetchart@ucdavis.edu>
2024-01-23 09:32:07 -0800

    new

---
+++ quiz1.sh
@@ -0,0 +1,6 @@
+Ashley Betchart
+ashleybetchar
+gunzip dictionary.gz -c |grep -xE "[muotfc] {1,}" | grep "a"
+gunzip dictionary.gz -c |grep -xE "[ailnrt] {1,}" | grep "b"
+gunzip dictionary.gz -c |grep -xE "[aonnidm] {1,}" | grep "c"
+gunzip dictionary.gz -c |grep -xE "[norgia] {1,}" | grep "z"
