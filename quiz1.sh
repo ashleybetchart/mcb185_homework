6ad3ced54c502d82d8f916ffb4c781c0d4f6a1ad
ashleybetchart <asbetchart@ucdavis.edu>
2024-01-23 09:40:43 -0800

    new

--- quiz1.sh
+++ quiz1.sh
@@ -4,3 +4,4 @@ gunzip dictionary.gz -c |grep -xE "[muotfc] {1,}" | grep "a"
 gunzip dictionary.gz -c |grep -xE "[ailnrt] {1,}" | grep "b"
 gunzip dictionary.gz -c |grep -xE "[aonnidm] {1,}" | grep "c"
 gunzip dictionary.gz -c |grep -xE "[norgia] {1,}" | grep "z"
+#coauthor Ashley, Aman
