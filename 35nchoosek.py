10d6a39174ac62568823f75f2e6c705ddd0feb03
ashleybetchart <asbetchart@ucdavis.edu>
2024-02-05 12:58:19 -0800

    new

--- 35nchoosek.py
+++ 35nchoosek.py
@@ -5,9 +5,12 @@ import math
 
 def factorial(n):
 	if n == 0: return 1
-	if k == 0: return 1
 	fac = 1
 	for i in range(1, n + 1):
 		fac = fac * i
-	factorial(n/k(n-k))
 	return fac
+print(factorial(3))
+
+def nchoosek(n, k):
+	return factorial(n) / (factorial(k)*(factorial(n-k)))
+print(nchoosek(3, 4))
