660c6ef98292a5d960f294029a6bae6ef58c7e54
ashleybetchart <asbetchart@ucdavis.edu>
2024-02-04 22:08:55 -0800

    new

---
+++ 35nchoosek.py
@@ -0,0 +1,13 @@
+# 35 nchoosek by Ashley Betchart
+# Co-authors: Ashley, Aman
+
+import math
+
+def factorial(n):
+	if n == 0: return 1
+	if k == 0: return 1
+	fac = 1
+	for i in range(1, n + 1):
+		fac = fac * i
+	factorial(n/k(n-k))
+	return fac
