181f75247709c8ae61e878bf2e6c1bd0798ec2e5
ashleybetchart <asbetchart@ucdavis.edu>
2024-02-06 09:24:26 -0800

    new

--- 38quiz.py
+++ 38quiz.py
@@ -11,5 +11,11 @@ for i in range(1, 201, 2):
 	n = n + 2
 	sign = sign * (-1)
 	print(4 * pi)
-
-
+	pi = 3
+	n = 2
+	sign = 1
+	for i in range(10):
+		pi = pi + (sign) * (4 / (n * (n+1) * (n+2)))
+		n = n + 2
+		sign = sign * (-1)
+		print(pi)
