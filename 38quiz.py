6bf74ee902933fa80b864924790ea99bf331186e
ashleybetchart <asbetchart@ucdavis.edu>
2024-02-06 09:21:17 -0800

    new

--- 38quiz.py
+++ 38quiz.py
@@ -1,2 +1,15 @@
 # quiz 3 by Ashley Betchart
 # Co-Author: Ashley, Aman
+
+import math
+
+pi = 1
+n = 1
+sign = -1
+for i in range(1, 201, 2):
+	pi = pi + ((sign) * (1 /(n + 2)))
+	n = n + 2
+	sign = sign * (-1)
+	print(4 * pi)
+
+
