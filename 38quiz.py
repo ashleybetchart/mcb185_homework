5f8cbfdfa5bc1e8ef7b17a39070e78c70d7db085
ashleybetchart <asbetchart@ucdavis.edu>
2024-02-06 09:32:36 -0800

    new

--- 38quiz.py
+++ 38quiz.py
@@ -19,3 +19,22 @@ for i in range(1, 201, 2):
 		n = n + 2
 		sign = sign * (-1)
 		print(pi)
+
+
+
+pia = 1
+na = 1
+signa = -1
+for i in range(1, 201, 2):
+	pia = pia + ((signa) * (1 /(n + 2)))
+	na = na + 2
+	signa = signa * (-1)
+	print(4 * pia)
+	pib = 3
+	nb = 2
+	signb = 1
+	for i in range(10):
+		pib = pib + (signb) * (4 / (n * (n+1) * (n+2)))
+		nb = nb + 2
+		signb = signb * (-1)
+		print(pib)
